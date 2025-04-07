43b2e100643f652323b4cc7e5a5205b081b433ee
Kendi <muriungikendileila@gmail.com>
2025-04-07 16:56:40 +0300

    adding the step state and conditional rendering for the messages

--- src/componentss2/counter.component.jsx
+++ src/componentss2/counter.component.jsx
@@ -2,18 +2,33 @@ import { useState } from 'react';
 
 export default function Counter() {
     const [count, setCount] = useState(0);
+    const [step, setStep] = useState(1);
 
-    const increment = () => setCount(count + 1);
-    const decrement = () => setCount(count - 1);
+    const increment = () => setCount(count + Number(step));
+    const decrement = () => setCount(count - Number(step));
     const reset = () => setCount(0);
 
     return (
-        <div>
+        <div className='counter'>
             <div className='counter-card'>  
-                <h2>Counter</h2>
-                <p>Count: {count}</p>
+                <h1>Counter</h1>
+                <h2 >Count: <span className='count'>{count}</span></h2>
             </div>
-          
+            <p>
+                {
+                    count < 0 ? "Going low!" :
+                    count >= 5 && count <= 10 ? "Getting there!" :
+                    count > 10 ? "Too high!":
+                    ""
+                }
+            </p>
+            <input 
+                type='number'
+                value={step}
+                onChange={(e) => setStep(e.target.value)}
+                placeholder='Step'
+            
+            />
             <button onClick={increment}>Increment</button>
             <button onClick={decrement}>Decrement</button>
             <button onClick={reset}>Reset</button>
